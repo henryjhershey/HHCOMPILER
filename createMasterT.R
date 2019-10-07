51000d99939136bd23f6595ad24a7182fae7b9aa
Henry Hershey <34039859+henryjhershey@users.noreply.github.com>
2019-10-07 18:37:19 -0500

    Add files via upload

---
+++ createMasterT.R
@@ -0,0 +1,35 @@
+
+#' createMasterT
+#'
+#' This function allows you to compile data from batches containing triangulated position text files.
+#' @param drive The single-letter name of the drive where the database is located. Usually "G" or "E" depending on which computer you are working from.
+#' @param batch The name or names of the batch folders where the position text files are stored. Must be an "array" batch.
+#' @keywords telemetry
+#' @export
+#' @examples
+#' createMasterT(drive="G",batch=c("UC ARRAY","LC ARRAY))
+
+
+createMasterT  <-  function(drive,batch){ 
+  
+  path1 = paste(drive,":/USACEFISHPASS/DATA/TELEMETRY/BATCHES/",batch,"/POSITIONS",sep="")
+  print(path1)
+  
+  library(plyr)
+  read_csv_filename <- function(filename){
+    ret       <- read.csv(filename,stringsAsFactors = F)
+    ret$tagID <- unlist(strsplit(filename, split = "_", fixed = T))[1] #EDIT
+    ret
+  }
+  
+  suppressWarnings(for(i in 1:length(path1)){
+    filenames   <- list.files(path1[i])
+    
+    #apply read.csv to all filenames in working directory then row bind csvs together
+    temp_mast <- ldply(filenames, read_csv_filename)
+    if (i == 1) mast = temp_mast else mast = rbind(mast,temp_mast)
+  })
+  
+  return(mast)
+  
+}
